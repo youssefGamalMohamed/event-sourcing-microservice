13e705da537ec750b480eb6633c11ecf8b995535
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-08 15:30:33 +0300

    feat: add auditing fields to Product entity for tracking creation and modification

--- product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/models/Product.java
+++ product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/models/Product.java
@@ -5,6 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
@@ -12,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
 
 
@@ -27,4 +35,18 @@ public class Product {
 
     private int quantity;
 
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime creationDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
