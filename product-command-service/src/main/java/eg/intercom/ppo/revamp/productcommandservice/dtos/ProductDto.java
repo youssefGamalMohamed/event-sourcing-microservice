941b3cacf576192ef9249b0e80e127680e045229
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-08 16:06:49 +0300

    feat: add Swagger annotations to ProductDto for API documentation

--- product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/dtos/ProductDto.java
+++ product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/dtos/ProductDto.java
@@ -1,9 +1,12 @@
 package eg.intercom.ppo.revamp.productcommandservice.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+
 import java.time.LocalDateTime;
 
 @Builder
+@Schema(description = "Product Data Transfer Object", name = "Product")
 public record ProductDto(
         String id,
         String name,
