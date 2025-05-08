8132428c2ebb700fc180c68cce736a81676f137d
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-08 16:05:51 +0300

    feat: update ProductMapper to ignore auditing fields during entity mapping

--- product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/mappers/ProductMapper.java
+++ product-command-service/src/main/java/eg/intercom/ppo/revamp/productcommandservice/mappers/ProductMapper.java
@@ -3,6 +3,7 @@ package eg.intercom.ppo.revamp.productcommandservice.mappers;
 import eg.intercom.ppo.revamp.productcommandservice.dtos.ProductDto;
 import eg.intercom.ppo.revamp.productcommandservice.models.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -13,7 +14,17 @@ public interface ProductMapper {
 
     ProductDto toDto(Product product);
 
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "lastModifiedBy", ignore = true)
+    @Mapping(target = "id", ignore = true)
     Product toEntity(ProductDto productDto);
 
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "lastModifiedBy", ignore = true)
+    @Mapping(target = "id", ignore = true)
     void updateFrom(Product source, @MappingTarget Product target);
 }
