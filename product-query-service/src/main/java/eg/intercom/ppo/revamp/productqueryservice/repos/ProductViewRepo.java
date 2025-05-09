c231b850632f98bb296047318ba8a862a08bae75
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-10 00:45:05 +0300

    feat: add method to find the most recent ProductView by originalId

--- product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
+++ product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductViewRepo extends MongoRepository<ProductView, String> {
 
     Page<ProductView> findAllByOriginalId(String originalId, Pageable pageable);
+
+    Optional<ProductView> findFirstByOriginalIdOrderByLastModifiedDateDesc(String originalId);
 }
