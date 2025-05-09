b9e154422b3f4e411ac0f7b451ad0e54f5a89cb5
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-09 18:31:20 +0300

    feat: add pagination support to ProductViewRepo with findAllByOriginalId method

--- product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
+++ product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
@@ -1,9 +1,13 @@
 package eg.intercom.ppo.revamp.productqueryservice.repos;
 
 import eg.intercom.ppo.revamp.productqueryservice.models.ProductView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductViewRepo extends MongoRepository<ProductView, String> {
+
+    Page<ProductView> findAllByOriginalId(String originalId, Pageable pageable);
 }
