752692bf8e1639ca691cae9ad8f342154760ca18
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-09 18:07:25 +0300

    refactor: change id type in ProductViewRepo from UUID to String

--- product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
+++ product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/repos/ProductViewRepo.java
@@ -4,8 +4,6 @@ import eg.intercom.ppo.revamp.productqueryservice.models.ProductView;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ProductViewRepo extends MongoRepository<ProductView, UUID> {
+public interface ProductViewRepo extends MongoRepository<ProductView, String> {
 }
