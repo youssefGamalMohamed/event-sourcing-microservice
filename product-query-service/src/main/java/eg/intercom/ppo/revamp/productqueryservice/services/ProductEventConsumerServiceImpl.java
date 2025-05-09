30980dddffcae2cebe2891d39530c9f7e4cd5aa5
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-09 18:08:34 +0300

    feat: integrate ProductViewService and ProductViewMapper in ProductEventConsumerServiceImpl for processing product events

--- product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/services/ProductEventConsumerServiceImpl.java
+++ product-query-service/src/main/java/eg/intercom/ppo/revamp/productqueryservice/services/ProductEventConsumerServiceImpl.java
@@ -1,6 +1,7 @@
 package eg.intercom.ppo.revamp.productqueryservice.services;
 
 import eg.intercom.ppo.revamp.productcommandservice.events.ProductEvent;
+import eg.intercom.ppo.revamp.productqueryservice.models.ProductView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ProductEventConsumerServiceImpl {
 
+    private final ProductViewService productViewService;
+    private final ProductViewMapper productViewMapper;
+
+    public ProductEventConsumerServiceImpl(ProductViewService productViewService, ProductViewMapper productViewMapper) {
+        this.productViewService = productViewService;
+        this.productViewMapper = productViewMapper;
+    }
 
     @KafkaListener(topics = "products-event", groupId = "product-group")
     public void consumeProductEvent(ProductEvent productEvent) {
         log.info("Received product event: {}", productEvent);
+        // Save Received Product Event to DB
+        productViewService.addProductView(productViewMapper.toProductView(productEvent));
     }
 
 
