28f8bd18141433cd5d39a354f4ef280d8ea97318
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-05-10 15:27:35 +0300

    feat: initialize api-gateway project with Maven wrapper and basic configuration

---
+++ api-gateway/src/test/java/eg/intercom/ppo/revamp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.intercom.ppo.revamp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
