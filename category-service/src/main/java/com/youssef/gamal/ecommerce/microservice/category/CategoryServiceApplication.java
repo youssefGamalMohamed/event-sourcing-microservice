40af51de27ebc59e67a6a04c24893abff7be5ca7
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-08-25 21:47:02 +0300

    chore: added category-service initalized project

---
+++ category-service/src/main/java/com/youssef/gamal/ecommerce/microservice/category/CategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.ecommerce.microservice.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+
+}
---
+++ category-service/src/test/java/com/youssef/gamal/ecommerce/microservice/category/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.ecommerce.microservice.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
