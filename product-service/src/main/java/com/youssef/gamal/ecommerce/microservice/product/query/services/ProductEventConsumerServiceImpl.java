e2714037c7605856e609b178d2878fe1c0981afb
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-08-31 01:53:03 +0300

    feat(product-service): use kafka instead of rabbitmq

--- product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/query/services/ProductEventConsumerServiceImpl.java
+++ product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/query/services/ProductEventConsumerServiceImpl.java
@@ -2,7 +2,8 @@ package com.youssef.gamal.ecommerce.microservice.product.query.services;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.kafka.annotation.KafkaListener;
 //import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +22,8 @@ public class ProductEventConsumerServiceImpl {
         this.productViewMapper = productViewMapper;
     }
 
-//    @KafkaListener(topics = "${kafka.event-names.product-event}", groupId = "${spring.kafka.consumer.group-id}")
-    @RabbitListener(queues = {"${rabbitmq.queues-names.product_queue}"})
+    @KafkaListener(topics = "${kafka.event-names.product-event}", groupId = "${spring.kafka.consumer.group-id}")
+//    @RabbitListener(queues = {"${rabbitmq.queues-names.product_queue}"})
     public void consumeProductEvent(ProductEvent productEvent) {
         log.info("✅ Received product event: {}", productEvent);
         // Save Received Product Event to DB
