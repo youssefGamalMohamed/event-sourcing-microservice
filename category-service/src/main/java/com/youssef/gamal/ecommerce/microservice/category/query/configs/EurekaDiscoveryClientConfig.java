0702a1e20820ac90d76449a4f6fc68b3696d4897
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-02 23:29:05 +0300

    feat(category-service): connected with discovery-service

---
+++ category-service/src/main/java/com/youssef/gamal/ecommerce/microservice/category/query/configs/EurekaDiscoveryClientConfig.java
@@ -0,0 +1,9 @@
+package com.youssef.gamal.ecommerce.microservice.category.query.configs;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaDiscoveryClientConfig {
+}
