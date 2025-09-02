fcad592d036ecbfaf86cd2b3419072292e8296e6
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-02 21:17:02 +0300

    refactor(shared-module): removed @SpringBootApplication annotations as it's not runnable app

--- shared-module/src/main/java/com/youssef/gamal/ecommerce/microservice/shared/module/ShartModuleApplication.java
+++ shared-module/src/main/java/com/youssef/gamal/ecommerce/microservice/shared/module/ShartModuleApplication.java
@@ -1,13 +1,8 @@
 package com.youssef.gamal.ecommerce.microservice.shared.module;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ShartModuleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShartModuleApplication.class, args);
 	}
 
 }
