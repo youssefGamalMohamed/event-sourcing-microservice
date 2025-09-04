c03b9bb4fe459bad99298047cb0910c44936a50e
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-05 01:30:10 +0300

    chore(product-service): remove unwanted codes

--- product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/common/configs/EurekaDiscoveryClientConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.youssef.gamal.ecommerce.microservice.product.common.configs;
-//
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@EnableDiscoveryClient
-//public class EurekaDiscoveryClientConfig {
-//}
--- product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/common/configs/FeignClientConfig.java
+++
@@ -1,8 +0,0 @@
-package com.youssef.gamal.ecommerce.microservice.product.common.configs;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FeignClientConfig {
-}
