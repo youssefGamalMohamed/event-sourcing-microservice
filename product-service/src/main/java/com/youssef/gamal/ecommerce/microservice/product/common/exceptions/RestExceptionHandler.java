be632e2738b639d0f863aa05ad7da80189097481
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-06 01:20:36 +0300

    feat(product-service -> RestExceptionHandler.java): added handling for most of exception

--- product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/common/exceptions/RestExceptionHandler.java
+++ product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/common/exceptions/RestExceptionHandler.java
@@ -1,53 +1,68 @@
-//package com.youssef.gamal.ecommerce.microservice.product.common.exceptions;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.ErrorResponse;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//import java.time.LocalDateTime;
-//import java.util.NoSuchElementException;
-//
-//@RestControllerAdvice
-//@Slf4j
-//public class RestExceptionHandler {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponse> handleException(Exception ex, HttpServletRequest request) {
-//        log.error("Handling generic exception:", ex);
-//        return createErrorResponse(
-//                HttpStatus.INTERNAL_SERVER_ERROR,
-//                ProductServiceErrorsEnum.INTERNAL_SERVER_ERROR,
-//                request.getRequestURI()
-//        );
-//    }
-//
-//    @ExceptionHandler(NoSuchElementException.class)
-//    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
-//        log.error("Handling NoSuchElementException:", ex);
-//        return createErrorResponse(
-//                HttpStatus.NOT_FOUND,
-//                ProductServiceErrorsEnum.PRODUCT_NOT_FOUND,
-//                request.getRequestURI()
-//        );
-//    }
-//
-//    private ResponseEntity<ErrorResponse> createErrorResponse(
-//            HttpStatus status,
-//            ProductServiceErrorsEnum commandError,
-//            String path) {
-//
-//        ErrorResponse errorResponse = ErrorResponse.builder()
-//                .timestamp(LocalDateTime.now())
-//                .status(status.value())
-//                .message(commandError.getServiceErrorMessage())
-//                .serviceCode(String.valueOf(commandError.getServiceErrorCode()))
-//                .path(path)
-//                .build();
-//
-//        return ResponseEntity.status(status).body(errorResponse);
-//    }
-//}
+package com.youssef.gamal.ecommerce.microservice.product.common.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public InternalServerErrorResponse handleException(Exception ex, HttpServletRequest request) {
+        log.error("Handling generic exception:", ex);
+        return InternalServerErrorResponse.builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .detailedMessage(ex.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public NotFoundResponse handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+        log.error("Handling NoSuchElementException:", ex);
+        return NotFoundResponse.builder()
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .detailedMessage(ex.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationException(
+            MethodArgumentNotValidException ex, HttpServletRequest request) {
+        log.error("Handling validation exception:", ex);
+
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+
+        return ValidationErrorResponse.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .detailedMessage(message)
+                .build();
+    }
+
+
+    @ExceptionHandler(AlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ConflictErrorResponse handleCategoryAlreadyExistException(
+            AlreadyExistException ex, HttpServletRequest request) {
+        log.error("Handling Product AlreadyExistException:", ex);
+
+        return ConflictErrorResponse.builder()
+                .httpStatus(HttpStatus.CONFLICT)
+                .detailedMessage(ex.getMessage())
+                .build();
+    }
+}
