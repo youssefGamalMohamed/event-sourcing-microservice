e6bd30f7df0849438acff79e6df939eff0939037
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-06 01:21:12 +0300

    feat(product-service -> ProductRepo.java): added find by name

--- product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/command/repos/ProductRepo.java
+++ product-service/src/main/java/com/youssef/gamal/ecommerce/microservice/product/command/repos/ProductRepo.java
@@ -1,10 +1,14 @@
 package com.youssef.gamal.ecommerce.microservice.product.command.repos;
 
+import com.youssef.gamal.ecommerce.microservice.product.command.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.youssef.gamal.ecommerce.microservice.product.command.entities.Product;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, String> {
+
+    Optional<Product> findByName(String name);
+
 }
