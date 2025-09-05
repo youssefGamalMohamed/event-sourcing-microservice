e4bc259d7a114913160206505adfa350cdc01f8d
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-05 20:15:23 +0300

    feat(shared-module -> category -> commands): added validation CategoryCommandDto

--- shared-module/src/main/java/com/youssef/gamal/ecommerce/microservice/shared/module/rest/dtos/category/commands/CategoryCommandDto.java
+++ shared-module/src/main/java/com/youssef/gamal/ecommerce/microservice/shared/module/rest/dtos/category/commands/CategoryCommandDto.java
@@ -1,15 +1,19 @@
 package com.youssef.gamal.ecommerce.microservice.shared.module.rest.dtos.category.commands;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record CategoryCommandDto (
+public record CategoryCommandDto(
         String id,
+        @NotBlank(message = "Name cannot be blank")
+        @Size(max = 255, message = "Name must be at most 255 characters")
         String name,
         LocalDateTime creationDate,
         String createdBy,
         LocalDateTime lastModifiedDate,
         String lastModifiedBy
-
 ) implements Serializable {
 }
