1e122456d0da46e55022af95f30d203b820149c4
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-09-05 22:29:41 +0300

    feat(category-service -> RestExceptionHandler.java): implemented with the new exception handling

--- category-service/src/main/java/com/youssef/gamal/ecommerce/microservice/category/common/exceptions/RestExceptionHandler.java
+++ category-service/src/main/java/com/youssef/gamal/ecommerce/microservice/category/common/exceptions/RestExceptionHandler.java
@@ -1,15 +1,13 @@
 package com.youssef.gamal.ecommerce.microservice.category.common.exceptions;
 
-import com.youssef.gamal.ecommerce.microservice.shared.module.rest.dtos.exceptions.models.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
@@ -18,29 +16,28 @@ import java.util.stream.Collectors;
 public class RestExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception ex, HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public InternalServerErrorResponse handleException(Exception ex, HttpServletRequest request) {
         log.error("Handling generic exception:", ex);
-        return createErrorResponse(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                CategoryServiceErrorsEnum.INTERNAL_SERVER_ERROR,
-                CategoryServiceErrorsEnum.INTERNAL_SERVER_ERROR.toString(),
-                request.getRequestURI()
-        );
+        return InternalServerErrorResponse.builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .detailedMessage(ex.getMessage())
+                .build();
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public NotFoundResponse handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
         log.error("Handling NoSuchElementException:", ex);
-        return createErrorResponse(
-                HttpStatus.NOT_FOUND,
-                CategoryServiceErrorsEnum.CATEGORY_NOT_FOUND,
-                ex.getMessage(),
-                request.getRequestURI()
-        );
+        return NotFoundResponse.builder()
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .detailedMessage(ex.getMessage())
+                .build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationException(
             MethodArgumentNotValidException ex, HttpServletRequest request) {
         log.error("Handling validation exception:", ex);
 
@@ -49,45 +46,23 @@ public class RestExceptionHandler {
                 .collect(Collectors.joining(", "));
 
 
-        return createErrorResponse(
-                HttpStatus.BAD_REQUEST,
-                CategoryServiceErrorsEnum.CATEGORY_REQUEST_BODY_FAILED_VALIDATION,
-                message,
-                request.getRequestURI()
-        );
+        return ValidationErrorResponse.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .detailedMessage(message)
+                .build();
     }
 
 
-    @ExceptionHandler(CategoryAlreadyExistException.class)
-    public ResponseEntity<ErrorResponse> handleCategoryAlreadyExistException(
-            CategoryAlreadyExistException ex, HttpServletRequest request) {
+    @ExceptionHandler(AlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ConflictErrorResponse handleCategoryAlreadyExistException(
+            AlreadyExistException ex, HttpServletRequest request) {
         log.error("Handling CategoryAlreadyExistException:", ex);
 
-        return createErrorResponse(
-                HttpStatus.CONFLICT, // 409
-                CategoryServiceErrorsEnum.CATEGORY_NAME_ALREADY_EXISTS,
-                ex.getMessage(), // "Category Already Exists with Name = X"
-                request.getRequestURI()
-        );
-    }
-
-
-    private ResponseEntity<ErrorResponse> createErrorResponse(
-            HttpStatus status,
-            CategoryServiceErrorsEnum errorsEnum,
-            String detailedMessage,
-            String path
-    ) {
-
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .status(status.value())
-                .message(errorsEnum.getServiceErrorMessage())
-                .detailedMessage(detailedMessage)
-                .serviceCode(String.valueOf(errorsEnum.getServiceErrorCode()))
-                .path(path)
+        return ConflictErrorResponse.builder()
+                .httpStatus(HttpStatus.CONFLICT)
+                .detailedMessage(ex.getMessage())
                 .build();
-
-        return ResponseEntity.status(status).body(errorResponse);
     }
+
 }
